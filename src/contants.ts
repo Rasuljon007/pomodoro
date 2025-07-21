dbfac86742b4b539423dcdb9f0256f5d772fb2e9
Rasuljon007 <nabiyevabdurasul116@gmail.com>
2025-07-21 14:57:57 +0500

    pomodoro

---
+++ src/contants.ts
@@ -0,0 +1,14 @@
+export const types = {
+  POMODORO: {
+    parentClassName: 'bg-pomodoro',
+    time: 25 * 60
+  },
+  SHORT_BREAK: {
+    parentClassName: 'bg-short',
+    time: 5 * 60
+  },
+  LONG_BREAK: {
+    parentClassName: 'bg-long',
+    time: 15 * 60
+  }
+};
